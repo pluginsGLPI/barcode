371fada8fb5838e36acb0478f254662391ec5a68
tsmr <tsmr@349b9182-4a13-0410-896f-e5e9767dd1b3>
2007-04-01 22:29:09 +0000

    [plusieurs] corrections

--- trunk/locales/fr_FR.php
+++ trunk/locales/fr_FR.php
@@ -1,36 +1,36 @@
 <?php
 /*
-
    ----------------------------------------------------------------------
    GLPI - Gestionnaire Libre de Parc Informatique
    Copyright (C) 2003-2005 by the INDEPNET Development Team.
 
    http://indepnet.net/   http://glpi.indepnet.org
-
    ----------------------------------------------------------------------
+
    LICENSE
 
-   This program is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public License (GPL)
-   as published by the Free Software Foundation; either version 2
-   of the License, or (at your option) any later version.
+   This file is part of GLPI.
 
-   This program is distributed in the hope that it will be useful,
+   GLPI is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   GLPI is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
 
-   To read the license please visit http://www.gnu.org/copyleft/gpl.html
-   ----------------------------------------------------------------------
+   You should have received a copy of the GNU General Public License
+   along with GLPI; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+   ------------------------------------------------------------------------
  */
-
 // Original Author of file: GRISARD Jean Marc
 // Purpose of file:
 // ----------------------------------------------------------------------
 $title = "Codes barres";
 
-
-//Français
 $LANGBARSCODE["title"][0] = "Faire un code barre...";
 $LANGBARSCODE["title"][1]	= "".$title."";
 $LANGBARSCODE["title"][2] = "Configuration de mise en page";
