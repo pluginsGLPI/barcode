3016ef1e0f050b1ec065e78e83de8439d8a93925
Remi Collet <remicollet@users.noreply.github.com>
2008-01-11 17:30:16 +0000

    [tous] - big dico clean (by yllen : Tsmr apero !)

--- front/plugin_barscode.config.php
+++ front/plugin_barscode.config.php
@@ -39,7 +39,7 @@ include (GLPI_ROOT . "/inc/includes.php");
 
 if(!isset($_SESSION["glpi_plugin_barscode_installed"]) || $_SESSION["glpi_plugin_barscode_installed"]!=1) {
 	
-	commonHeader($LANG["title"][2],$_SERVER['PHP_SELF'],"config","plugins");
+	commonHeader($LANG["common"][12],$_SERVER['PHP_SELF'],"config","plugins");
 	
 	if ($_SESSION["glpiactive_entity"]==0){
 	
