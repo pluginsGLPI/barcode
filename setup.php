efa7b08462c148be32af259c5facb42f6ed6cc78
Xavier CAILLAUD <tsmr@users.noreply.github.com>
2008-09-01 00:02:23 +0000

    [pleins] correction display

--- setup.php
+++ setup.php
@@ -214,7 +214,6 @@ function plugin_headings_actions_barscode($type){
 
 function plugin_user_preferences_barscode(){
 	global $DB;
-print_r($_POST);
 
 	$pref = new plugin_barscode_UserPreferences;
 	$pref->showForm($_SERVER["PHP_SELF"],$_POST);
