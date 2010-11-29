a41c4584030a203b1b0e841df4af3ff1e9538b0f
David Durieux <ddurieux@users.noreply.github.com>
2010-11-29 22:20:50 +0000

    Version 2.0.0 for glpi 0.78.x

---
+++ front/config.php
@@ -0,0 +1,52 @@
+<?php
+/*
+ -------------------------------------------------------------------------
+ GLPI - Gestionnaire Libre de Parc Informatique
+ Copyright (C) 2003-2010 by the INDEPNET Development Team.
+
+ http://indepnet.net/   http://glpi-project.org
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of GLPI.
+
+ GLPI is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ GLPI is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ --------------------------------------------------------------------------
+ */
+
+// ----------------------------------------------------------------------
+// Original Author of file: Vincent Mazzoni
+// Purpose of file:
+// ----------------------------------------------------------------------
+
+// Non menu entry case
+//header("Location:../../central.php");
+
+// Entry menu case
+define('GLPI_ROOT', '../../..');
+include (GLPI_ROOT . "/inc/includes.php");
+
+checkRight("config","w");
+
+// To be available when plugin is not activated
+Plugin::load('barcode');
+
+commonHeader($LANG['plugin_barcode']["name"],$_SERVER['PHP_SELF'],"config","plugins");
+$barcode = new PluginBarcodeBarcode();
+$barcode->showFormConfig('');
+
+commonFooter();
+?>
