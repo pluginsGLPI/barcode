d1f2f4f45af95f0d61c0070a61aa3689a8ad9690
ddurieux <ddurieux@349b9182-4a13-0410-896f-e5e9767dd1b3>
2016-08-25 20:31:51 +0000

    Release 0.90+1.0

---
+++ tags/0.90+1.0/hook.php
@@ -0,0 +1,188 @@
+<?php
+
+/*
+   ------------------------------------------------------------------------
+   Barcode
+   Copyright (C) 2009-2016 by the Barcode plugin Development Team.
+
+   https://forge.indepnet.net/projects/barscode
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of barcode plugin project.
+
+   Plugin Barcode is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Plugin Barcode is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Plugin Barcode. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   Plugin Barcode
+   @author    David Durieux
+   @co-author
+   @copyright Copyright (c) 2009-2016 Barcode plugin Development team
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      https://forge.indepnet.net/projects/barscode
+   @since     2009
+
+   ------------------------------------------------------------------------
+ */
+
+// Define actions :
+function plugin_barcode_MassiveActions($type) {
+
+   switch ($type) {
+      // New action for core and other plugin types : name = plugin_PLUGINNAME_actionname
+      case 'Computer' :
+      case 'Monitor' :
+		case 'Networking' :
+		case 'Printer' :
+		case 'Peripheral' :
+		case 'Phone' :
+         return array("PluginBarcodeBarcode".MassiveAction::CLASS_ACTION_SEPARATOR.'Generate' => __('Barcode', 'barcode')." - ".__('Print barcodes', 'barcode'),
+                      "PluginBarcodeQRcode".MassiveAction::CLASS_ACTION_SEPARATOR.'Generate'  => __('Barcode', 'barcode')." - ".__('Print QRcodes', 'barcode'));
+         
+		case 'Ticket' :
+         return array("PluginBarcodeBarcode".MassiveAction::CLASS_ACTION_SEPARATOR.'Generate' => __('Barcode', 'barcode')." - ".__('Print barcodes', 'barcode'));
+
+//      case 'Profile' :
+//         return array("plugin_barcode_allow" => __('Barcode', 'barcode'));
+   }
+   return array();
+}
+
+
+
+// Install process for plugin : need to return true if succeeded
+function plugin_barcode_install() {
+   global $DB;
+
+   $migration = new Migration(PLUGIN_BARCODE_VERSION);
+   
+   if (!file_exists(GLPI_PLUGIN_DOC_DIR."/barcode")) {
+      mkdir(GLPI_PLUGIN_DOC_DIR."/barcode");
+   }
+   $migration->renameTable("glpi_plugin_barcode_config", "glpi_plugin_barcode_configs");
+   if (!TableExists("glpi_plugin_barcode_configs")) {
+      $query = "CREATE TABLE `glpi_plugin_barcode_configs` (
+                  `id` int(11) NOT NULL auto_increment,
+                  `type` varchar(20) collate utf8_unicode_ci default NULL,
+                  PRIMARY KEY  (`ID`)
+               ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci";
+      $DB->query($query) or die("error creating glpi_plugin_barcode_configs ". $DB->error());
+
+      $query = "INSERT INTO `glpi_plugin_barcode_configs` 
+                     (`id`, `type`)
+                VALUES
+                     ('1', 'code128')";
+      $DB->query($query) or die("error populate glpi_plugin_barcode_configs ". $DB->error());
+   }
+
+   $migration->renameTable("glpi_plugin_barcode_config_type", "glpi_plugin_barcode_configs_types");
+   if (!TableExists("glpi_plugin_barcode_configs_types")) {
+      $query = "CREATE TABLE `glpi_plugin_barcode_configs_types` (
+                  `id` int(11) NOT NULL auto_increment,
+                  `type` varchar(20) collate utf8_unicode_ci default NULL,
+                  `size` varchar(20) collate utf8_unicode_ci default NULL,
+                  `orientation` varchar(9) collate utf8_unicode_ci default NULL,
+                  `marginTop` int(11) NULL,
+                  `marginBottom` int(11) NULL,
+                  `marginLeft` int(11) NULL,
+                  `marginRight` int(11) NULL,
+                  `marginHorizontal` int(11) NULL,
+                  `marginVertical` int(11) NULL,
+                  `maxCodeWidth` int(11) NULL,
+                  `maxCodeHeight` int(11) NULL,
+                  PRIMARY KEY  (`ID`),
+                  UNIQUE  (`type`)
+               ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+      $DB->query($query) or die("error creating glpi_plugin_barcode_configs_types ". $DB->error());
+
+      $query = "INSERT INTO `glpi_plugin_barcode_configs_types`
+                     (`type`, `size`, `orientation`,
+                     `marginTop`, `marginBottom`, `marginLeft`, `marginRight`,
+                     `marginHorizontal`, `marginVertical`, `maxCodeWidth`, `maxCodeHeight`)
+                VALUES
+                     ('Code39', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '128', '50'),
+                     ('code128', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '70'),
+                     ('ean13', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '70'),
+                     ('int25', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '70'),
+                     ('postnet', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '70'),
+                     ('upca', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '70'),
+                     ('QRcode', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '100')";
+      $DB->query($query) or die("error populate glpi_plugin_barcode_configs_types ". $DB->error());
+   }
+   
+   if (countElementsInTable("glpi_plugin_barcode_configs_types",
+                            "`type`='QRcode'") == 0) {
+      $query = "INSERT INTO `glpi_plugin_barcode_configs_types`
+                     (`type`, `size`, `orientation`,
+                     `marginTop`, `marginBottom`, `marginLeft`, `marginRight`,
+                     `marginHorizontal`, `marginVertical`, `maxCodeWidth`, `maxCodeHeight`)
+                VALUES
+                     ('QRcode', 'A4', 'Portrait',
+                     '30', '30', '30', '30',
+                     '25', '30', '110', '100')";
+      $DB->query($query) or die("error populate glpi_plugin_barcode_configs_types ". $DB->error());
+   }
+
+   include_once GLPI_ROOT.'/plugins/barcode/inc/profile.class.php';
+   include_once GLPI_ROOT.'/plugins/barcode/inc/config.class.php';
+   PluginBarcodeProfile::initProfile();
+   if (TableExists("glpi_plugin_barcode_profiles")) {
+      $query = "DROP TABLE `glpi_plugin_barcode_profiles`";
+      $DB->query($query) or die("error deleting glpi_plugin_barcode_profiles");
+   }
+   return true;
+}
+
+
+
+// Uninstall process for plugin : need to return true if succeeded
+function plugin_barcode_uninstall() {
+   global $DB;
+
+   if (TableExists("glpi_plugin_barcode_configs")) {
+      $query = "DROP TABLE `glpi_plugin_barcode_configs`";
+      $DB->query($query) or die("error deleting glpi_plugin_barcode_configs");
+   }
+   if (TableExists("glpi_plugin_barcode_configs_types")) {
+      $query = "DROP TABLE `glpi_plugin_barcode_configs_types`";
+      $DB->query($query) or die("error deleting glpi_plugin_barcode_configs_types");
+   }
+   if (TableExists("glpi_plugin_barcode_profiles")) {
+      $query = "DROP TABLE `glpi_plugin_barcode_profiles`";
+      $DB->query($query) or die("error deleting glpi_plugin_barcode_profiles");
+   }
+   
+   include_once GLPI_ROOT.'/plugins/barcode/inc/profile.class.php';
+   PluginBarcodeProfile::removeRights();
+
+   return true;
+}
+?>
---
+++ tags/0.90+1.0/inc/profile.class.php
@@ -0,0 +1,247 @@
+<?php
+
+/*
+   ------------------------------------------------------------------------
+   Barcode
+   Copyright (C) 2009-2016 by the Barcode plugin Development Team.
+
+   https://forge.indepnet.net/projects/barscode
+   ------------------------------------------------------------------------
+
+   LICENSE
+
+   This file is part of barcode plugin project.
+
+   Plugin Barcode is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   Plugin Barcode is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with Plugin Barcode. If not, see <http://www.gnu.org/licenses/>.
+
+   ------------------------------------------------------------------------
+
+   @package   Plugin Barcode
+   @author    David Durieux
+   @co-author
+   @copyright Copyright (c) 2009-2016 Barcode plugin Development team
+   @license   AGPL License 3.0 or (at your option) any later version
+              http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   @link      https://forge.indepnet.net/projects/barscode
+   @since     2009
+
+   ------------------------------------------------------------------------
+ */
+
+class PluginBarcodeProfile extends Profile {
+
+   static $rightname = "config";
+
+
+   function getTabNameForItem(CommonGLPI $item, $withtemplate=0) {
+      if ($item->getID() > 0
+              && $item->fields['interface'] == 'central') {
+         return self::createTabEntry(__('Barcode', 'barcode'));
+      }
+   }
+
+
+
+   static function displayTabContentForItem(CommonGLPI $item, $tabnum=1, $withtemplate=0) {
+      $pfProfile = new self();
+      $pfProfile->showForm($item->getID());
+      return TRUE;
+   }
+
+
+
+    /**
+    * Show profile form
+    *
+    * @param $items_id integer id of the profile
+    * @param $target value url of target
+    *
+    * @return nothing
+    **/
+   function showForm($profiles_id=0, $openform=TRUE, $closeform=TRUE) {
+
+      echo "<div class='firstbloc'>";
+      if (($canedit = Session::haveRightsOr(self::$rightname, array(CREATE, UPDATE, PURGE)))
+          && $openform) {
+         $profile = new Profile();
+         echo "<form method='post' action='".$profile->getFormURL()."'>";
+      }
+
+      $profile = new Profile();
+      $profile->getFromDB($profiles_id);
+
+      $rights = $this->getAllRights();
+      $profile->displayRightsChoiceMatrix($rights, array('canedit'       => $canedit,
+                                                      'default_class' => 'tab_bg_2',
+                                                      'title'         => __('Barcode', 'barcode')));
+      if ($canedit
+          && $closeform) {
+         echo "<div class='center'>";
+         echo Html::hidden('id', array('value' => $profiles_id));
+         echo Html::submit(_sx('button', 'Save'), array('name' => 'update'));
+         echo "</div>\n";
+         Html::closeForm();
+      }
+      echo "</div>";
+   }
+
+
+
+   static function uninstallProfile() {
+      $pfProfile = new self();
+      $a_rights = $pfProfile->getAllRights();
+      foreach ($a_rights as $data) {
+         ProfileRight::deleteProfileRights(array($data['field']));
+      }
+   }
+
+
+
+   function getAllRights() {
+      $a_rights = array(
+          array('rights'    => array(UPDATE  => __('Update')),
+                'label'     => __('Manage configuration', 'barcode'),
+                'field'     => 'plugin_barcode_config'
+          ),
+          array('rights'    => array(CREATE  => __('Create')),
+                'label'     => __('Generation of barcode', 'barcode'),
+                'field'     => 'plugin_barcode_barcode'
+          )
+      );
+      return $a_rights;
+   }
+
+
+
+   static function addDefaultProfileInfos($profiles_id, $rights) {
+      $profileRight = new ProfileRight();
+      foreach ($rights as $right => $value) {
+         if (!countElementsInTable('glpi_profilerights',
+                                   "`profiles_id`='$profiles_id' AND `name`='$right'")) {
+            $myright['profiles_id'] = $profiles_id;
+            $myright['name']        = $right;
+            $myright['rights']      = $value;
+            $profileRight->add($myright);
+
+            //Add right to the current session
+            $_SESSION['glpiactiveprofile'][$right] = $value;
+         }
+      }
+   }
+
+   /**
+    * @param $profiles_id  integer
+    */
+   static function createFirstAccess($profiles_id) {
+      include_once(GLPI_ROOT."/plugins/barcode/inc/profile.class.php");
+      $profile = new self();
+      foreach ($profile->getAllRights() as $right) {
+         self::addDefaultProfileInfos($profiles_id,
+                                      array($right['field'] => ALLSTANDARDRIGHT));
+      }
+   }
+
+   static function removeRights() {
+      $profile = new self();
+      foreach ($profile->getAllRights() as $right) {
+         if (isset($_SESSION['glpiactiveprofile'][$right['field']])) {
+            unset($_SESSION['glpiactiveprofile'][$right['field']]);
+         }
+         ProfileRight::deleteProfileRights(array($right['field']));
+      }
+   }
+
+   static function migrateProfiles() {
+      //Get all rights from the old table
+      $profiles = getAllDatasFromTable(getTableForItemType(__CLASS__));
+
+      //Load mapping of old rights to their new equivalent
+      $oldrights = self::getOldRightsMappings();
+
+      //For each old profile : translate old right the new one
+      foreach ($profiles as $id => $profile) {
+         switch ($profile['right']) {
+            case 'r' :
+               $value = READ;
+               break;
+            case 'w':
+               $value = ALLSTANDARDRIGHT;
+               break;
+            case 0:
+            default:
+               $value = 0;
+               break;
+         }
+         //Write in glpi_profilerights the new fusioninventory right
+         if (isset($oldrights[$profile['type']])) {
+            //There's one new right corresponding to the old one
+            if (!is_array($oldrights[$profile['type']])) {
+               self::addDefaultProfileInfos($profile['profiles_id'],
+                                            array($oldrights[$profile['type']] => $value));
+            } else {
+               //One old right has been splitted into serveral new ones
+               foreach ($oldrights[$profile['type']] as $newtype) {
+                  self::addDefaultProfileInfos($profile['profiles_id'],
+                                               array($newtype => $value));
+               }
+            }
+         }
+      }
+   }
+
+
+
+   /**
+   * Init profiles during installation :
+   * - add rights in profile table for the current user's profile
+   * - current profile has all rights on the plugin
+   */
+   static function initProfile() {
+      $pfProfile = new self();
+      $profile   = new Profile();
+      $a_rights  = $pfProfile->getAllRights();
+
+      foreach ($a_rights as $data) {
+         if (countElementsInTable("glpi_profilerights", "`name` = '".$data['field']."'") == 0) {
+            ProfileRight::addProfileRights(array($data['field']));
+            $_SESSION['glpiactiveprofile'][$data['field']] = 0;
+         }
+      }
+
+      // Add all rights to current profile of the user
+      if (isset($_SESSION['glpiactiveprofile'])) {
+         $dataprofile       = array();
+         $dataprofile['id'] = $_SESSION['glpiactiveprofile']['id'];
+         $profile->getFromDB($_SESSION['glpiactiveprofile']['id']);
+         foreach ($a_rights as $info) {
+            if (is_array($info)
+                && ((!empty($info['itemtype'])) || (!empty($info['rights'])))
+                  && (!empty($info['label'])) && (!empty($info['field']))) {
+
+               if (isset($info['rights'])) {
+                  $rights = $info['rights'];
+               } else {
+                  $rights = $profile->getRightsFor($info['itemtype']);
+               }
+               foreach (array_keys($rights) as $right) {
+                  $dataprofile['_'.$info['field']][$right] = 1;
+                  $_SESSION['glpiactiveprofile'][$data['field']] = $right;
+               }
+            }
+         }
+         $profile->update($dataprofile);
+      }
+   }
+}
+?>
