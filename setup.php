b014dda7cfd748e90a3bade7a6ab591cc6a94b34
Cédric Anne <cedric.anne@gmail.com>
2020-02-06 16:00:33 +0100

    Add 2.4.1 version definition

--- setup.php
+++ setup.php
@@ -39,7 +39,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_BARCODE_VERSION", "2.4.0");
+define ("PLUGIN_BARCODE_VERSION", "2.4.1");
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_BARCODE_MIN_GLPI', '9.4');
