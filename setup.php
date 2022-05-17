4e431b23560d0eb136904ddad6da7d821a491e39
Cédric Anne <cedric.anne@gmail.com>
2022-05-17 14:47:30 +0200

    2.7.0

--- setup.php
+++ setup.php
@@ -39,7 +39,7 @@
    ------------------------------------------------------------------------
  */
 
-define ("PLUGIN_BARCODE_VERSION", "2.6.2");
+define ("PLUGIN_BARCODE_VERSION", "2.7.0");
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_BARCODE_MIN_GLPI', '10.0.0');
